83698593e960ffe6ea7b66653c3066b247d09783
liutianyu <liutianyu@wlan-197042.nbw.tue.nl>
2015-11-23 15:41:16 +0100

    *Finish constructing robot v1.0 *Could add some more parts

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -90,8 +90,6 @@ class Robot {
     private int RightLegNormalPtr;
     private FloatBuffer RightLegNormals;
     
-    
-    
     private boolean Initialized = false;
     
     /**
@@ -144,16 +142,15 @@ class Robot {
 //    }
     
     /*Determine points of Head*/
-    private void Head(GL2 gl)
-    {
+    private void head(GL2 gl) {
         float[] vertexArray = {-0.05f,  0.05f, 1f,
-                                0.05f,  0.05f, 1f,
-                                0.05f, -0.05f, 1f,
-                                -0.05f, -0.05f, 1f,
-                                -0.05f, 0.05f, 0.9f,
-                                0.05f, 0.05f, 0.9f,
-                                0.05f, -0.05f, 0.9f,
-                                -0.05f, -0.05f, 0.9f};
+                               0.05f,  0.05f, 1f,
+                               0.05f, -0.05f, 1f,
+                               -0.05f, -0.05f, 1f,
+                               -0.05f, 0.05f, 0.9f,
+                               0.05f, 0.05f, 0.9f,
+                               0.05f, -0.05f, 0.9f,
+                               -0.05f, -0.05f, 0.9f};
         HeadVertices = Buffers.newDirectFloatBuffer(vertexArray);
 
         short[] indexArray = {0, 1, 2, 
@@ -203,16 +200,15 @@ class Robot {
     }
     
     /*Determine points of torso*/
-    private void Torso(GL2 gl)
-    {
-        float[] vertexArray = {-0.2f, 0.06f, 0.89f, 
-                                0.2f, 0.06f, 0.89f,
-                                0.2f, -0.06f, 0.89f,
-                                -0.2f, -0.06f, 0.89f,
-                                -0.2f, 0.06f, 0.55f,
-                                0.2f, 0.06f, 0.55f, 
-                                0.2f, -0.06f, 0.55f,
-                                -0.2f, -0.06f, 0.55f};
+    private void torso(GL2 gl) {
+        float[] vertexArray = {-0.15f, 0.06f, 0.9f, 
+                               0.15f, 0.06f, 0.9f,
+                               0.15f, -0.06f, 0.9f,
+                               -0.15f, -0.06f, 0.9f,
+                               -0.15f, 0.06f, 0.5f,
+                               0.15f, 0.06f, 0.5f, 
+                               0.15f, -0.06f, 0.5f,
+                               -0.15f, -0.06f, 0.5f};
         TorsoVertices = Buffers.newDirectFloatBuffer(vertexArray);
 
         short[] indexArray = {0, 1, 2, 
@@ -260,7 +256,268 @@ class Robot {
                             TorsoNormals, gl.GL_STATIC_DRAW);
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
     }
+    
+    /*Determine points of left arm and right arm*/
+    private void leftArm(GL2 gl) {
+        float[] vertexArray = {-0.16f, -0.06f, 0.9f, 
+                               -0.26f, -0.06f, 0.9f,
+                               -0.26f, 0.06f, 0.9f,
+                               -0.16f, 0.06f, 0.9f, 
+                               -0.16f, -0.06f, 0.52f, 
+                               -0.26f, -0.06f, 0.52f, 
+                               -0.26f, 0.06f, 0.64f, 
+                               -0.16f, 0.06f, 0.64f, 
+                               -0.16f, 0.18f, 0.52f, 
+                               -0.26f, 0.18f, 0.52f, 
+                               -0.26f, 0.18f, 0.64f, 
+                               -0.16f, 0.18f, 0.64f};
+        LeftArmVertices = Buffers.newDirectFloatBuffer(vertexArray);
 
+        short[] indexArray = {0, 1, 2, 
+                              0, 2, 3,
+                              0, 1, 5, 
+                              0, 5, 4, 
+                              1, 2, 6, 
+                              1, 6, 5, 
+                              2, 3, 7, 
+                              2, 7, 6, 
+                              3, 0, 4, 
+                              3, 4, 7, 
+                              4, 5, 9, 
+                              4, 9, 8, 
+                              5, 6, 10, 
+                              5, 10, 9, 
+                              6, 7, 11, 
+                              6, 11, 10, 
+                              7, 4, 8, 
+                              7, 8, 9, 
+                              8, 9, 10, 
+                              8, 10, 11};
+        LeftArmIndices = Buffers.newDirectShortBuffer(indexArray);
+        
+        float[] normalArray ={0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1};
+        LeftArmNormals = Buffers.newDirectFloatBuffer(normalArray);
+        
+        int[] temp = new int[3];
+        gl.glGenBuffers(3, temp, 0);
+
+        LeftArmVertexPtr = temp[0];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftArmVertexPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, LeftArmVertices.capacity() * Buffers.SIZEOF_FLOAT,
+                            LeftArmVertices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+
+        LeftArmIndexPtr = temp[1];
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, LeftArmIndexPtr);
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, LeftArmIndices.capacity() * Buffers.SIZEOF_SHORT,
+                            LeftArmIndices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, 0);
+        
+        LeftArmNormalPtr = temp[2];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER,LeftArmNormalPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, LeftArmNormals.capacity() * Buffers.SIZEOF_FLOAT,
+                            LeftArmNormals, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+    }
+    
+    private void rightArm(GL2 gl) {
+        float[] vertexArray = {0.16f, -0.06f, 0.9f, 
+                               0.26f, -0.06f, 0.9f,
+                               0.26f, 0.06f, 0.9f,
+                               0.16f, 0.06f, 0.9f, 
+                               0.16f, -0.06f, 0.52f, 
+                               0.26f, -0.06f, 0.52f, 
+                               0.26f, 0.06f, 0.64f, 
+                               0.16f, 0.06f, 0.64f, 
+                               0.16f, 0.18f, 0.52f, 
+                               0.26f, 0.18f, 0.52f, 
+                               0.26f, 0.18f, 0.64f, 
+                               0.16f, 0.18f, 0.64f};
+        RightArmVertices = Buffers.newDirectFloatBuffer(vertexArray);
+
+        short[] indexArray = {0, 1, 2, 
+                              0, 2, 3,
+                              0, 1, 5, 
+                              0, 5, 4, 
+                              1, 2, 6, 
+                              1, 6, 5, 
+                              2, 3, 7, 
+                              2, 7, 6, 
+                              3, 0, 4, 
+                              3, 4, 7, 
+                              4, 5, 9, 
+                              4, 9, 8, 
+                              5, 6, 10, 
+                              5, 10, 9, 
+                              6, 7, 11, 
+                              6, 11, 10, 
+                              7, 4, 8, 
+                              7, 8, 9, 
+                              8, 9, 10, 
+                              8, 10, 11};
+        RightArmIndices = Buffers.newDirectShortBuffer(indexArray);
+        
+        float[] normalArray ={0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1};
+        RightArmNormals = Buffers.newDirectFloatBuffer(normalArray);
+        
+        int[] temp = new int[3];
+        gl.glGenBuffers(3, temp, 0);
+
+        RightArmVertexPtr = temp[0];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightArmVertexPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, RightArmVertices.capacity() * Buffers.SIZEOF_FLOAT,
+                            RightArmVertices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+
+        RightArmIndexPtr = temp[1];
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, RightArmIndexPtr);
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, RightArmIndices.capacity() * Buffers.SIZEOF_SHORT,
+                            RightArmIndices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, 0);
+        
+        RightArmNormalPtr = temp[2];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER,RightArmNormalPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, RightArmNormals.capacity() * Buffers.SIZEOF_FLOAT,
+                            RightArmNormals, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+    }
+    
+    /*Determine points of left leg and right leg*/
+    private void leftLeg(GL2 gl) {
+        float[] vertexArray = {-0.15f, 0.045f, 0.49f, 
+                               -0.05f, 0.045f, 0.49f, 
+                               -0.05f, -0.045f, 0.49f, 
+                               -0.15f, -0.045f, 0.49f, 
+                               -0.15f, 0.045f, 0.05f, 
+                               -0.05f, 0.045f, 0.05f, 
+                               -0.05f, -0.045f, 0.05f, 
+                               -0.15f, -0.045f, 0.05f};
+        LeftLegVertices = Buffers.newDirectFloatBuffer(vertexArray);
+
+        short[] indexArray = {0, 1, 2, 
+                              0, 2, 3, 
+                              0, 5, 1, 
+                              0, 5, 4, 
+                              1, 6, 2, 
+                              1, 6, 5, 
+                              2, 7, 3, 
+                              2, 7, 6, 
+                              3, 4, 0, 
+                              3, 4, 7,
+                              4, 5, 6, 
+                              4, 6, 7};
+        LeftLegIndices = Buffers.newDirectShortBuffer(indexArray);
+        
+        float[] normalArray ={0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1};
+        LeftLegNormals = Buffers.newDirectFloatBuffer(normalArray);
+        
+        int[] temp = new int[3];
+        gl.glGenBuffers(3, temp, 0);
+
+        LeftLegVertexPtr = temp[0];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftLegVertexPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, LeftLegVertices.capacity() * Buffers.SIZEOF_FLOAT,
+                            LeftLegVertices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+
+        LeftLegIndexPtr = temp[1];
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, LeftLegIndexPtr);
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, LeftLegIndices.capacity() * Buffers.SIZEOF_SHORT,
+                            LeftLegIndices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, 0);
+        
+        LeftLegNormalPtr = temp[2];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftLegNormalPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, LeftLegNormals.capacity() * Buffers.SIZEOF_FLOAT,
+                            LeftLegNormals, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+    }
+    
+    private void rightLeg(GL2 gl) {
+        float[] vertexArray = {0.15f, 0.045f, 0.49f, 
+                               0.05f, 0.045f, 0.49f, 
+                               0.05f, -0.045f, 0.49f, 
+                               0.15f, -0.045f, 0.49f, 
+                               0.15f, 0.045f, 0.05f, 
+                               0.05f, 0.045f, 0.05f, 
+                               0.05f, -0.045f, 0.05f, 
+                               0.15f, -0.045f, 0.05f};
+        RightLegVertices = Buffers.newDirectFloatBuffer(vertexArray);
+
+        short[] indexArray = {0, 1, 2, 
+                              0, 2, 3, 
+                              0, 5, 1, 
+                              0, 5, 4, 
+                              1, 6, 2, 
+                              1, 6, 5, 
+                              2, 7, 3, 
+                              2, 7, 6, 
+                              3, 4, 0, 
+                              3, 4, 7,
+                              4, 5, 6, 
+                              4, 6, 7};
+        RightLegIndices = Buffers.newDirectShortBuffer(indexArray);
+        
+        float[] normalArray ={0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1, 
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1,
+                              0, 0, 1};
+        RightLegNormals = Buffers.newDirectFloatBuffer(normalArray);
+        
+        int[] temp = new int[3];
+        gl.glGenBuffers(3, temp, 0);
+
+        RightLegVertexPtr = temp[0];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightLegVertexPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, RightLegVertices.capacity() * Buffers.SIZEOF_FLOAT,
+                          RightLegVertices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+
+        RightLegIndexPtr = temp[1];
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, RightLegIndexPtr);
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, RightLegIndices.capacity() * Buffers.SIZEOF_SHORT,
+                            RightLegIndices, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, 0);
+        
+        RightLegNormalPtr = temp[2];
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightLegNormalPtr);
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, RightLegNormals.capacity() * Buffers.SIZEOF_FLOAT,
+                            RightLegNormals, gl.GL_STATIC_DRAW);
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+    }
     /**
      * Draws this robot (as a {@code stickfigure} if specified).
      */
@@ -273,10 +530,22 @@ class Robot {
 //        }
         
         /*Ptrs head*/
-        Head(gl);
+        head(gl);
         
         /*Ptrs torso*/
-        Torso(gl);
+        torso(gl);
+        
+        /*Ptrs left arm*/
+        leftArm(gl);
+        
+        /*Ptrs right arm*/
+        rightArm(gl);
+        
+        /*Ptrs left leg*/
+        leftLeg(gl);
+        
+        /*Ptrs right leg*/
+        rightLeg(gl);
         
         /*Sheding function*/ 
         sheding(gl);
@@ -294,6 +563,18 @@ class Robot {
         
         /*Draw Torso*/
         drawTorso(gl);
+        
+        /*Draw left arm*/
+        drawLeftArm(gl);
+        
+        /*Draw right arm*/
+        drawRightArm(gl);
+        
+        /*Draw left leg*/
+        drawLeftLeg(gl);
+        
+        /*Draw right leg*/
+        drawRightLeg(gl);
 
         /*Finish drawing, disable client state*/
         gl.glDisableClientState(gl.GL_VERTEX_ARRAY);
@@ -333,7 +614,7 @@ class Robot {
 //    }
     
     /*Function to draw head of the robot*/
-    private void drawHead(GL2 gl){
+    private void drawHead(GL2 gl) {
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, HeadVertexPtr);
         gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
         
@@ -348,7 +629,7 @@ class Robot {
     }
     
     /*Function to draw torso of the robot*/
-    private void drawTorso(GL2 gl){
+    private void drawTorso(GL2 gl) {
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, TorsoVertexPtr);
         gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
         
@@ -360,5 +641,65 @@ class Robot {
         
         gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, TorsoIndexPtr);
         gl.glDrawElements(gl.GL_TRIANGLES, HeadIndices.capacity(), gl.GL_UNSIGNED_SHORT, 0);
+    }
+    
+    /*Function to draw left arm of the robot*/
+    private void drawLeftArm(GL2 gl) {
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftArmVertexPtr);
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        //gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftArmTexcoordPtr);
+        //gl.glTexCoordPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftArmNormalPtr);
+        gl.glNormalPointer(gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, LeftArmIndexPtr);
+        gl.glDrawElements(gl.GL_TRIANGLES, LeftArmIndices.capacity(), gl.GL_UNSIGNED_SHORT, 0);
+    }
+    
+    /*Function to draw right arm of the robot*/
+    private void drawRightArm(GL2 gl) {
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightArmVertexPtr);
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        //gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightArmTexcoordPtr);
+        //gl.glTexCoordPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightArmNormalPtr);
+        gl.glNormalPointer(gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, RightArmIndexPtr);
+        gl.glDrawElements(gl.GL_TRIANGLES, RightArmIndices.capacity(), gl.GL_UNSIGNED_SHORT, 0);
+    }
+    
+    /*Draw left leg of the robot*/
+    private void drawLeftLeg(GL2 gl) {
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftLegVertexPtr);
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        //gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftLegTexcoordPtr);
+        //gl.glTexCoordPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, LeftLegNormalPtr);
+        gl.glNormalPointer(gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, LeftLegIndexPtr);
+        gl.glDrawElements(gl.GL_TRIANGLES, LeftLegIndices.capacity(), gl.GL_UNSIGNED_SHORT, 0);
+    }
+    
+    /*Draw right leg of the robot*/
+    private void drawRightLeg(GL2 gl) {
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightLegVertexPtr);
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        //gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightLegTexcoordPtr);
+        //gl.glTexCoordPointer(3, gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, RightLegNormalPtr);
+        gl.glNormalPointer(gl.GL_FLOAT, 0, 0);
+        
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, RightLegIndexPtr);
+        gl.glDrawElements(gl.GL_TRIANGLES, RightLegIndices.capacity(), gl.GL_UNSIGNED_SHORT, 0);
     }
 }
